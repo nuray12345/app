1231e75bc8bb482712546177c5f177de5969c41c
Nurayim <erkinbekovanuray159263@bk.ru>
2025-03-11 11:59:07 +0600

    Cart входит в компоненты

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './front/components/Header/Header';
 import Parallax from './front/components/Paralax/Paralax.jsx';
 import { useState } from 'react';
-
+import Cart from './front/components/Cart/CardFooter/Cart/index'
 export default function App() {
     const [content, setContent] = useState('Tap the button');
 
@@ -13,6 +13,7 @@ export default function App() {
         <div>
             <Header handleClick={handleClick} />  {/* Pass handleClick as prop */}
             <Parallax/>
+            <Cart/>
         </div>
     );
 }
---
+++ src/front/components/Cart/CardFooter/CardFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CardFooter.sass'
+
+const CartFooter = ({ total }) => {
+    return (
+        <footer className='cart-footer'>
+
+        </footer>
+    );
+};
+
+export default CartFooter;
